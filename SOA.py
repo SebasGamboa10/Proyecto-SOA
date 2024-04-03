1de82fceb99779a666b8be326c1797dba39a7311
JosefRuzicka <jose.f.2000@hotmail.com>
2024-04-03 13:24:44 -0600

    creación de logs por proceso

--- SOA.py
+++ SOA.py
@@ -13,6 +13,7 @@ class Process:
         self.send_block = send_block
         self.receive_block = receive_block
         self.message_queue = []
+        self.log = {}
 
     def send_message(self, message, destination, blocking=None):
         if blocking=="blocking":
@@ -35,7 +36,7 @@ class Process:
         elif self.message_queue:
             return self.message_queue.pop(0)
         else:
-            return "No hay mansajes disponibles"
+            return "No hay mensajes disponibles"
 
 
 def create_process(name, pid, send_block, receive_block):
@@ -48,6 +49,7 @@ def display(processes):
         print(f"Nombre del proceso: {proceso.name}")
         print(f"Bool Send: {proceso.send_block}")
         print(f"Bool receive: {proceso.receive_block}")
+        print(f"log: {proceso.log}")
 
 def configure_system():
     print("Configuración del sistema:")
@@ -55,13 +57,14 @@ def configure_system():
     message_queue_size = int(input("Ingrese el tamaño de la cola de mensajes: "))
     send_type = str(input("El send es blocking o nonblocking: "))
     receive_type = str(input("El receive es blocking o nonblocking: "))
+
     return SystemConfiguration(num_processes, message_queue_size,send_type, receive_type)
 
 def main():
     processes = {}
     config = configure_system()
-
-    while True:
+    time = 0
+    while True: 
         command = input("Ingrese un comando (create, send, receive), 'display' para ver el estado o 'exit' para salir: ")
 
         if command == "exit":
@@ -72,10 +75,11 @@ def main():
             if config.num_processes > len(processes): 
                 name = input("Ingrese el nombre del proceso: ")
                 process_id = input("Ingrese el ID del proceso: ")
-                send_block = None
+                send_block = None # aqui sería send_block = send_type? lo mismo abajo, bueno con un if blocking entonces true, y else non blocking
                 receive_block = None
 
                 new_process = create_process(name, process_id, send_block, receive_block)
+                new_process.log[time] = 'Proceso creado'
                 processes[new_process.pid] = new_process
             else:
                 print("No es posible agregar procesos, se ha alcanzado el maximo")
@@ -89,10 +93,11 @@ def main():
             elif process_id not in processes:
                 print(f"Proceso {process_id} no existe")
             else:
-                send_block = processes[process_id_send].send_block
+                send_block = processes[process_id_send].send_block # el comentario de arriba asumiendo que todos los procs tienen el mismo tipo
 
                 if send_block is False or send_block is None:
                     message = input("Ingrese el mensaje: ")
+                    processes[process_id_send].log[time] =  'Envié mensaje {} a proc {}'.format(message, process_id)
                     if config.send_type == 'blocking':
                         processes[process_id_send].send_block = True
                         processes[process_id_send].send_message(f"{message}", processes[process_id], blocking=config.receive_type)
@@ -110,15 +115,19 @@ def main():
                 received_message = processes[process_id].receive_message_blocking()
                 if received_message:
                     print(f"Process {process_id} recibió el mensaje: {received_message}")
+                    processes[process_id].log[time] = 'Recibí mensaje {}'.format(received_message)
                 else:
                     print(f"Process {process_id} no recibió ningún mensaje.")
             else:
                 received_message = processes[process_id].receive_message_nonblocking()
                 print(f"Process {process_id} recibió el mensaje: {received_message}")
+                processes[process_id].log[time] = 'Recibí mensaje {}'.format(received_message)
 
 
         elif command == "display":
             display(processes)
+
+        time += 1
 
 if __name__ == "__main__":
     main()

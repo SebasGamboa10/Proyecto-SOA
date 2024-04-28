9fbd834cd9ec528dfc71569dc834d51ceb84d062
JosefRuzicka <jose.f.2000@hotmail.com>
2024-04-27 20:18:55 -0600

    removed unused vars

--- PR2_Scheduling/SOA.py
+++ PR2_Scheduling/SOA.py
@@ -9,15 +9,11 @@ def print_procs(procs):
     for proc in procs:
         print(f"(PID: {proc.pid}, deadline: {proc.deadline}, time period: {proc.time_period}), remaining time: {proc.remaining_time}")  
     
-
 procs = [Process(1,4,1),Process(2,5,2),Process(3,7,2)]
 #print_procs(procs)
 
 # procs: list of processes, each a tuple (or dict) of (ID, time_period, deadline, time_remaining)
-# sched_time = LCM of the time period of all procs. #TODO: verificar con el prof.
-def rate_monotonic_scheduling(procs, sched_time = None, steps = 15):
-    active_procs = []
-    
+def rate_monotonic_scheduling(procs, steps = 15):
     # simulate
     for i in range(steps):
         print("------- Iter: ", i, "-------")
@@ -32,10 +28,10 @@ def rate_monotonic_scheduling(procs, sched_time = None, steps = 15):
                 #print_procs([proc])
 
         # schedule proc
-        # (shortest time_period first)
         unfinished_procs = [proc for proc in procs if proc.remaining_time > 0]
         print_procs(unfinished_procs)
-        if unfinished_procs:    
+        if unfinished_procs:
+            # (shortest time_period first)    
             current_proc = min(unfinished_procs, key=lambda x: x.time_period)
             current_proc.remaining_time -= 1
             print(f"Giving CPU to pid: {current_proc.pid}, rem: {current_proc.remaining_time}")
